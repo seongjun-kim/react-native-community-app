803ba5615befeb8e5fa076bfc49f4b83a6db6de7
seongjun_kim <lewisel@naver.com>
2021-03-13 00:21:11 +0900

    feat: Loading Page Animation 추가

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import React, {useState} from 'react';
-import {SafeAreaView, StatusBar} from 'react-native';
-import Loading from './components/contents/Loading';
-import Main from './components/contents/Main';
+import React, { useState } from "react";
+import { SafeAreaView, StatusBar } from "react-native";
+import Loading from "./components/contents/Loading";
+import Main from "./components/contents/Main";
 
 const App = () => {
   const [loadingState, setLoadingState] = useState(false);
 
   setTimeout(() => {
-    console.log('change loading state!');
+    console.log("change loading state!");
     setLoadingState(true);
-  }, 1000);
+  }, 5000);
 
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>{loadingState ? <Main /> : <Loading />}</SafeAreaView>
+      <SafeAreaView style={{ flex: 1 }}>
+        {loadingState ? <Main /> : <Loading />}
+      </SafeAreaView>
     </>
   );
 };
--- components/contents/Loading.js
+++ components/contents/Loading.js
@@ -1,24 +1,69 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, { useEffect, useRef, useState } from "react";
+import { StyleSheet, Dimensions, Text, View, Animated } from "react-native";
+import * as Progress from "react-native-progress";
 
+const w = Dimensions.get("window");
+const imageUrl = "http://clipart-library.com/2018-gallery/donut-clipart-13.png";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+let t = 0.1;
 const Loading = () => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 5000,
+    }).start();
+  }, []);
+
+  setTimeout(() => {
+    const intervalObj = setInterval(() => {
+      setProgress(t);
+      t += 0.1;
+      if (progress >= 1) {
+        clearInterval(intervalObj);
+      }
+    }, 200);
+  }, 2000);
+
   return (
-    <>
-      <View
+    <View style={styles.container}>
+      <Animated.Image
+        source={{
+          uri: `${imageUrl}?w=10&buster=${Math.random()}`,
+        }}
+        style={[
+          {
+            width: w.width / 2,
+            height: w.width / 2,
+            resizeMode: "contain",
+            marginBottom: 100,
+            opacity: fadeAnim,
+          },
+        ]}
+        onLoad={this.onImageLoad}
+      />
+      <Text
         style={{
-          justifyContent: 'center',
-          backgroundColor: '#000000',
-          height: '100%',
-        }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            color: '#ffffff',
-          }}>
-          Loading
-        </Text>
-      </View>
-    </>
+          fontSize: 20,
+          fontWeight: "bold",
+          color: "black",
+          marginBottom: 100,
+        }}
+      >
+        LOGO
+      </Text>
+      <Progress.Bar progress={progress} width={200} />
+    </View>
   );
 };
 
